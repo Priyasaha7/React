a7e30b155dd4b94b1221488435103c0447801db0
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2025-08-09 16:45:29 +0530

    Day 05: Started my React learning series

---
+++ Day05/src/components/Body.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import RestaurantCard from './RestaurantCard';
+import resList from '../utils/mockData';
+
+const Body = () => {
+
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+    return(
+        <div className="body">
+
+            <div className="search-container">
+                <button className="filter-btn"
+                 onClick={()=>{
+                    const filterRestaurent = listOfRestaurants.filter((res) => res.data.avgRating > 4);
+                    setListOfRestaurants(filterRestaurent);
+                    console.log(setListOfRestaurants);
+                }
+                }>Top Rated Restaurants</button>
+            </div>
+
+            <div className="res-container">
+                { listOfRestaurants.map((restaurant) => (
+                    <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+                )
+                )}
+            </div>
+
+        </div>
+
+    );  
+};
+
+export default Body;
+
